bb8bd07ed793e87782b7878d4f0a8c79e14180ad
bcurl21 <74270428+bcurl21@users.noreply.github.com>
2021-03-02 21:26:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,73 @@
-
     require([
-      "esri/WebScene",
-      "esri/views/SceneView",
-      "esri/Camera",
-      "esri/widgets/Home",
+	  "esri/Map",
+      "esri/views/MapView",
+	  "esri/layers/FeatureLayer",
       "dojo/domReady!"
-    ], function(WebScene, SceneView, Camera, Home) {
+    ], function(
+      Map,MapView, FeatureLayer
+    ) {
 
-    
-      /*var map = new Map({
-        basemap: "streets",
-        ground: "world-elevation"
-      });*/
-      var scene = new WebScene({
-        portalItem:{
-         id:"8046207c1c214b5587230f5e5f8efc77" 
-        }
-      });
+      /************************************************************
+       * Creates a new WebMap instance. A WebMap must reference
+       * a PortalItem ID that represents a WebMap saved to
+       * arcgis.com or an on-premise portal.
+       *
+       * To load a WebMap from an on-premise portal, set the portal
+       * url with esriConfig.portalUrl.
+       ************************************************************/
+	  const fl = new FeatureLayer({
+		url:"https://services2.arcgis.com/zNjnZafDYCAJAbN0/arcgis/rest/services/Traffic_Collisions/FeatureServer",
+      outFields: ["*"],
+      popupTemplate: template
+	  });
       
-      var camera = new Camera({
-        position: [
-           -71.060217,
-          42.322655,
-          4000// elevation in meters
-        ],
-        tilt:45,
-        heading: 0
-      })
+      var symbol = {
+  type: "simple-marker", 
+  color:"red"
+};
+var renderer = {
+  type: "simple",  // autocasts as new SimpleRenderer()
+  symbol: symbol
+};
+
+fl.renderer = renderer
+
+      /************************************************************
+       * Set the WebMap instance to the map property in a MapView.
+       ************************************************************/
+      var map = new Map({
+          basemap: "streets"
+        });
+
+        var view = new MapView({
+          container: "viewDiv",
+          map: map,
+		  extent: { // autocasts as new Extent()
+			xmin: -118.264858634618,
+			ymin: 33.5444932701483,
+			xmax: -117.299011374275,
+			ymax: 35.0054716855699,
+            spatialReference: 4326
+          }
+          
+        });
       
-      var camera2 = new Camera({
-        position: {
-          x: 116.4074,
-          y: 39.9042,
-          z: 5000000
-        },
-        tilt: 0,
-        heading: 0
-      });
-
-      var view = new SceneView({
-        container: "viewDiv",
-        map: scene,
-        viewingMode:"global",
-        camera: camera,
-        environment: {
-            lighting: {
-              date: new Date(),
-              directShadowsEnabled: true,
-              // don't update the view time when user pans.
-              // The clock widget drives the time
-              cameraTrackingEnabled: false
-            }
-        },
-    });
-    
-    /*var homeBtn = new Home({
-        view: view
-      });*/
-
-      // Add the home button to the top left corner of the view
-    view.ui.add(homeBtn, "top-left");
-    
-    [stl, bei].forEach(function(button) {
-      button.style.display = 'flex';
-      view.ui.add(button, 'top-right');
-    });
-    
-    bei.addEventListener('click', function() {
-      // reuse the default camera position already established in the homeBtn
-      view.goTo({
-        target:camera2
-      });
-    });
-    
-    stl.addEventListener('click', function() {
-      // reuse the default camera position already established in the homeBtn
-      view.goTo({
-        target:camera
-      });
+	  map.add(fl);
+	  
     });
 
+var template = { // autocasts as new PopupTemplate()
+        title: "What Happened?",
+        content: [{
+          // It is also possible to set the fieldInfos outside of the content
+          // directly in the popupTemplate. If no fieldInfos is specifically set
+          // in the content, it defaults to whatever may be set within the popupTemplate.
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "CollisnTyp",
+            label: "Collision Type",
+            visible: true
+          }]
+        }]
+      };
 
-    });
